a1fa23e427b3c848501487cb3dccba7d5f09e4f2
ardhiancalwa <ardhiancalwaofc@gmail.com>
2024-08-04 11:22:14 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -41,7 +41,7 @@ app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
