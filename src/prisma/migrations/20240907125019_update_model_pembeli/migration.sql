e5fc28266e9008cea3712a7c50c6d6622efd162d
ardhiancalwa <ardhiancalwaofc@gmail.com>
2024-09-07 19:57:19 +0700

    update model pembeli

---
+++ src/prisma/migrations/20240907125019_update_model_pembeli/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `alamat` on the `Pembeli` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Pembeli" DROP COLUMN "alamat";
