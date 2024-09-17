dec61605a750b632ee51ab87ea04d0a51f2114b0
ardhiancalwa <ardhiancalwaofc@gmail.com>
2024-09-17 09:05:28 +0700

    fixing bcrypt

--- src/services/pembeliService.js
+++ src/services/pembeliService.js
@@ -1,5 +1,5 @@
 const Pembeli = require("../models/pembeliModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const {
