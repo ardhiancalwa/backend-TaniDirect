316333132dc7d26808c7a710e93db91057a58826
ardhiancalwa <ardhiancalwaofc@gmail.com>
2024-09-17 20:20:08 +0700

    update pembeli model

--- src/models/pembeliModel.js
+++ src/models/pembeliModel.js
@@ -1,6 +1,7 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const cloudinary = require("../utils/cloudinary");
+const bcrypt = require("bcryptjs");
 
 const Pembeli = {
   findAll: async () => {
